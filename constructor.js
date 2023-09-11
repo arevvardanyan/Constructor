dac586e29576a9599cbbd33845bbdebb611959f0
Arev <vardanyanarevik23@gmail.com>
2023-09-11 13:44:22 +0400

    Person constructor with compare age method

--- constructor.js
+++ constructor.js
@@ -23,3 +23,38 @@ console.log(number.divide(10,5))
 console.log(number.add(10,5))
 console.log(number.multiply(10,5))
 console.log(number.subtract(10,5))
+
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// 2
+
+const Person = function(name,age){
+
+    this.name = name,
+    this.age = age
+    
+    getName = function(){
+        return this.name
+    }
+    getAge = function(){
+        return this.age
+    }
+}
+
+Person.prototype.compareAge = function(obj){
+    const {age} = obj
+    if(this.age > age){
+        return `${this.name} is older than me`
+    }else if(this.age === age){
+        return `${this.name} is the same age as me.`
+    }else{
+        return `${this.name} is younger than me`
+    }
+}
+
+const p1 = new Person("Samuel", 24)
+const p2 = new Person("Joel", 36)
+const p3 = new Person("Lily", 24)
+console.log(p1.compareAge(p2))
+console.log(p2.compareAge(p1))
+console.log(p1.compareAge(p3))
